e8dd5ec0a8a7a2fd69309ee2bf762116dfaf8e15
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-12-11 22:57:53 -0500

    i fix storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -7,7 +7,7 @@ const storage = ({ stream }) => {
         api_secret: process.env.CLOUDINARY_API_SECRET,
     });
     return new Promise((resolve, reject) => {
-        const buffer = cloudinary.v2.uploader_stream((err, result) => {
+        const buffer = cloudinary.v2.uploader.upload_stream((err, result) => {
             if(err) reject(err);
             resolve(result);
         });
